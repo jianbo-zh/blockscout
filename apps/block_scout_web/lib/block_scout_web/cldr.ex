7113592ba3e4fd40e5cda16db7f8927fdd000d5d
zjianbo <tipine@163.com>
2022-01-11 18:27:27 +0800

    inital

--- apps/block_scout_web/lib/block_scout_web/cldr.ex
+++ apps/block_scout_web/lib/block_scout_web/cldr.ex
@@ -4,8 +4,8 @@ defmodule BlockScoutWeb.Cldr do
   """
 
   use Cldr,
-    default_locale: "en",
-    locales: ["en"],
+    default_locale: "zh",
+    locales: ["zh", "en"],
     gettext: BlockScoutWeb.Gettext,
     generate_docs: false,
     precompile_number_formats: ["#,###", "#,##0.##################", "#.#%", "#,##0"],
